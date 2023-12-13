9eeb5b2c5316d9eed699aaea9e4c241e48ecd28c
Eric Nelson <ericwnelson84@gmail.com>
2023-12-13 06:08:59 +0000

    xray app.py code fixes

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,7 +20,7 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 # xray
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+
 
 # Honeycomb -----
 from opentelemetry import trace
@@ -39,6 +39,8 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
+# xray
+XRayMiddleware(app, xray_recorder)
 # For Honeycomb telemetry below. Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
