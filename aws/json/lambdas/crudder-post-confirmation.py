3f32dd757d2790bf04add70b05ad66159b5046c3
Eric Nelson <ericwnelson84@gmail.com>
2023-12-19 11:04:37 +0000

    in progress

--- aws/json/lambdas/crudder-post-confirmation.py
+++ aws/json/lambdas/crudder-post-confirmation.py
@@ -24,6 +24,7 @@ def lambda_handler(event, context):
       """
       print('SQL Statement ----')
       print(sql)
+      print(os.getenv('CONNECTION_URL'))
       conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
       cur = conn.cursor()
       params = [
@@ -32,7 +33,7 @@ def lambda_handler(event, context):
         user_handle,
         user_cognito_id
       ]
-      cur.execute(sql,*params)
+      cur.execute(sql,params)
       conn.commit() 
 
     except (Exception, psycopg2.DatabaseError) as error:
