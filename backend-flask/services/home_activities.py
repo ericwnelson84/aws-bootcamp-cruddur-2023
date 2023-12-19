1c0fedf016ae8f55f1bce7b4e4bd648870b6f4f1
Eric Nelson <ericwnelson84@gmail.com>
2023-12-19 07:23:17 +0000

    got backend to connect to db and diplay db info on homepage

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -8,7 +8,20 @@ class HomeActivities:
     # logger.info("HomeActivities")
     now = datetime.now(timezone.utc).astimezone()
     sql = query_wrap_array("""
-    SELECT * FROM activities
+      SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
     """)
     print(sql)
     with pool.connection() as conn:
@@ -17,7 +30,6 @@ class HomeActivities:
         # this will return a tuple
         # the first field being the data
         json = cur.fetchone()
-    print("testing-----------------")
     return json[0]
 
 
