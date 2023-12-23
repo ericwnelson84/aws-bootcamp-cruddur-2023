2882b2fea8303c53d53819a514fdc94515fdbc0d
Eric Nelson <ericwnelson84@gmail.com>
2023-12-23 16:20:53 +0000

    added healthy check enpoint for backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -116,21 +116,25 @@ def _build_request_data(request):
 rollbar._build_request_data = _build_request_data
 ## XXX end hack
 
-def init_rollbar(app):
-  rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-  flask_env = os.getenv('FLASK_ENV')
-  rollbar.init(
-      # access token
-      rollbar_access_token,
-      # environment name
-      flask_env,
-      # server root directory, makes tracebacks prettier
-      root=os.path.dirname(os.path.realpath(__file__)),
-      # flask already sets up logging
-      allow_logging_basic_config=False)
-  # send exceptions from `app` to rollbar, using flask's signal system.
-  got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-  return rollbar
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+
+# def init_rollbar(app):
+#   rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+#   flask_env = os.getenv('FLASK_ENV')
+#   rollbar.init(
+#       # access token
+#       rollbar_access_token,
+#       # environment name
+#       flask_env,
+#       # server root directory, makes tracebacks prettier
+#       root=os.path.dirname(os.path.realpath(__file__)),
+#       # flask already sets up logging
+#       allow_logging_basic_config=False)
+#   # send exceptions from `app` to rollbar, using flask's signal system.
+#   got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+#   return rollbar
 
 @app.route('/rollbar/test')
 def rollbar_test():
