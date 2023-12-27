bd9059ee2aa7e6f69ae76950c5474109cb98686c
Eric Nelson <ericwnelson84@gmail.com>
2023-12-27 04:18:25 +0000

    rollbar update

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -103,6 +103,10 @@ def after_request(response):
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
 
+@app.route('/api/health-check')
+def health_check():
+  return {'success': True}, 200
+
 ## XXX hack to make request data work with pyrollbar <= 0.16.3
 def _get_flask_request():
     print("Getting flask request")
@@ -116,25 +120,22 @@ def _build_request_data(request):
 rollbar._build_request_data = _build_request_data
 ## XXX end hack
 
-@app.route('/api/health-check')
-def health_check():
-  return {'success': True}, 200
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
 
-# def init_rollbar(app):
-#   rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-#   flask_env = os.getenv('FLASK_ENV')
-#   rollbar.init(
-#       # access token
-#       rollbar_access_token,
-#       # environment name
-#       flask_env,
-#       # server root directory, makes tracebacks prettier
-#       root=os.path.dirname(os.path.realpath(__file__)),
-#       # flask already sets up logging
-#       allow_logging_basic_config=False)
-#   # send exceptions from `app` to rollbar, using flask's signal system.
-#   got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-#   return rollbar
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.route('/rollbar/test')
 def rollbar_test():
