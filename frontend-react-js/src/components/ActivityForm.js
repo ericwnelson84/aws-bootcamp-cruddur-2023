828072f29d080f54e35f8634fec24663a03e9e4b
Eric Nelson <ericwnelson84@gmail.com>
2024-01-21 03:16:47 +0000

    added checkAuth verification to Activity form

--- frontend-react-js/src/components/ActivityForm.js
+++ frontend-react-js/src/components/ActivityForm.js
@@ -2,6 +2,7 @@ import './ActivityForm.css';
 import React from "react";
 import process from 'process';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { getAccessToken } from 'lib/CheckAuth';
 
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
@@ -18,12 +19,15 @@ export default function ActivityForm(props) {
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities`
-      console.log('onsubmit payload', message)
+      // console.log('onsubmit payload', message)
+      const access_token = await getAccessToken()
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
+
         },
         body: JSON.stringify({
           message: message,
