93819b94a6ddab6ff6b2f0af8763e5336b45ad20
Eric Nelson <ericwnelson84@gmail.com>
2024-01-23 01:50:53 +0000

    refactoring working so far

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -34,12 +34,6 @@ with app.app_context():
   g.rollbar = init_rollbar(app)
 
 
-def model_json(model):
-  if model['errors'] is not None:
-    return model['errors'], 422
-  else:
-    return model['data'], 200
-
 
 @app.route('/api/health-check')
 def health_check():
