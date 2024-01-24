a9186dffed3bfcee25ecaedbd6799a1936202f91
Eric Nelson <ericwnelson84@gmail.com>
2024-01-24 03:11:54 +0000

    update

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,4 +1,4 @@
--- this file was manually created
+
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
@@ -7,11 +7,15 @@ VALUES
   ('Wanna Sakulprom','sakulprom1982@gmail.com' , 'Nana' ,'MOCK'),
   ('Londo Mollari', 'lmollari@centari.com','londo','MOCK');
 
-
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'Nelly' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'Nana' LIMIT 1),
+    'I am Nana!',
+    current_timestamp + interval '10 day'
+  );
